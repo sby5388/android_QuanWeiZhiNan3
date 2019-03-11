fcd646a56a9b02c4136079280281d2c5a723a80b
shenbingyan <shenby@server03>
2019-03-11 11:22:20 +0800

    add ch16 1.pm 判断是否有处理Intent的Activity 2.ContentProvider的替代者 FileProvider, 使用方法 3.拍照，保存到软件私有空间 4.图片的压缩，

---
+++ 16_CameraIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+/**
+ * 图片处理工具
+ * @author Administrator
+ */
+public class PictureUtils {
+    /**
+     * Scale：比例,
+     */
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point point = new Point();
+        //作用是 set-point
+        activity.getWindowManager().getDefaultDisplay().getSize(point);
+        return getScaledBitmap(path, point.x, point.y);
+    }
+
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+        // Read in the dimensions of the image on disk
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        // Figure out how much to scale down by
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWidth;
+
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale :
+                    widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // Read in and create final bitmap
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
