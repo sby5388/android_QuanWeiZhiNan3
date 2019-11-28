06ca529d78d2e8ade274592d4f163ac1fb7dafc3
shenbingyan <shenby@server03>
2019-11-28 14:33:07 +0800

    chap06 finish pratice task

--- 06_AndroidVersions/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ 06_AndroidVersions/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -12,6 +12,8 @@ import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE =
@@ -23,6 +25,9 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
+    private TextView mTextViewRemainCheatCount;
+
+    private static int sCountCheat = 3;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -40,13 +45,20 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mTextViewRemainCheatCount = findViewById(R.id.show_count_remain);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (sCountCheat < 1) {
+                    return;
+                }
+                sCountCheat--;
+                setButtonEnable();
                 if (mAnswerIsTrue) {
                     mAnswerTextView.setText(R.string.true_button);
                 } else {
@@ -55,10 +67,16 @@ public class CheatActivity extends AppCompatActivity {
                 setAnswerShownResult(true);
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    // TODO: 2019/11/28 动画效果
+                    //  传入一些特定参数，createCircularReveal方法创建了一个Animator。首先，指定要显
+                    //  示或隐藏的View，然后是动画的中心位置、起始半径和结束半径。为隐藏按钮，这里将它的起始
+                    //  半径设置为按钮宽度，结束半径设置为0。
+                    //  动画启动前，设置一个监听器，确定动画何时播完。动画一结束，就显示答案并隐藏按钮。
+                    //  最后播放动画，看看效果。
                     int cx = mShowAnswerButton.getWidth() / 2;
                     int cy = mShowAnswerButton.getHeight() / 2;
                     float radius = mShowAnswerButton.getWidth();
-                    Animator anim = ViewAnimationUtils
+                    final Animator anim = ViewAnimationUtils
                             .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
                     anim.addListener(new AnimatorListenerAdapter() {
                         @Override
@@ -73,6 +91,17 @@ public class CheatActivity extends AppCompatActivity {
                 }
             }
         });
+        setButtonEnable();
+
+        final TextView textView = findViewById(R.id.show_build_version);
+        final int sdkInt = Build.VERSION.SDK_INT;
+        textView.setText(String.format(Locale.getDefault(), "API LEVEL %d", sdkInt));
+
+    }
+
+    private void setButtonEnable() {
+        mShowAnswerButton.setEnabled(sCountCheat > 0);
+        mTextViewRemainCheatCount.setText(String.format(Locale.getDefault(), "剩余查看次数:%d", sCountCheat));
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
