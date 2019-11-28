33e1851bb189b3d0ec73f4dbc678c86631dc039c
shenbingyan <shenby@server03>
2019-11-28 15:39:32 +0800

    chap 08 upgradle gradle-version

--- 08_RecyclerView/CriminalIntent8/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ 08_RecyclerView/CriminalIntent8/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -76,8 +78,9 @@ public class CrimeListFragment extends Fragment {
             mCrimes = crimes;
         }
 
+        @NonNull
         @Override
-        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             return new CrimeHolder(layoutInflater, parent);
         }
