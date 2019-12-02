3082d0e2e62894eb80e755e852265e26ca9cd82e
shenbingyan <shenby@server03>
2019-12-02 16:49:44 +0800

    chap 15 finish practive

--- 15_ImplicitIntents/CriminalIntent15/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ 15_ImplicitIntents/CriminalIntent15/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,17 +1,24 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +26,27 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Date;
 import java.util.UUID;
 
-import static android.widget.CompoundButton.*;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER;
+import static android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class CrimeFragment extends Fragment {
+    private static final String TAG = CrimeFragment.class.getSimpleName();
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
 
+    private static final int REQUEST_CODE_READ_CONTACTS = 100;
+
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_CONTACT = 1;
+    private static final int REQUEST_CALL = 2;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -39,6 +54,7 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckbox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private Button mCallButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -97,20 +113,38 @@ public class CrimeFragment extends Fragment {
         mSolvedCheckbox.setChecked(mCrime.isSolved());
         mSolvedCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, 
-                    boolean isChecked) {
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
                 mCrime.setSolved(isChecked);
             }
         });
 
+        mCallButton = v.findViewById(R.id.button_call);
+        mCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPhone();
+            }
+        });
+
+
+        // TODO: 2019/12/2 发送犯罪信息
         mReportButton = (Button) v.findViewById(R.id.crime_report);
         mReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
+                boolean useShareCompat = true;
+                if (useShareCompat) {
+                    useShareCompat();
+                    return;
+                }
+
                 Intent i = new Intent(Intent.ACTION_SEND);
                 i.setType("text/plain");
                 i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
                 i.putExtra(Intent.EXTRA_SUBJECT,
                         getString(R.string.crime_report_subject));
+                // TODO: 2019/12/2 每一次都是供用户选择，而不是使用上次的分享方式
                 i = Intent.createChooser(i, getString(R.string.send_report));
                 startActivity(i);
             }
@@ -118,6 +152,7 @@ public class CrimeFragment extends Fragment {
 
         final Intent pickContact = new Intent(Intent.ACTION_PICK,
                 ContactsContract.Contacts.CONTENT_URI);
+        // TODO: 2019/12/2 选择嫌疑人
         mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
         mSuspectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,21 +162,58 @@ public class CrimeFragment extends Fragment {
         });
         if (mCrime.getSuspect() != null) {
             mSuspectButton.setText(mCrime.getSuspect());
+            mCallButton.setVisibility(View.VISIBLE);
         }
 
         PackageManager packageManager = getActivity().getPackageManager();
         if (packageManager.resolveActivity(pickContact,
                 PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            // TODO: 2019/12/2 如果设备上没有默认的联系人提供者，则不可点击
             mSuspectButton.setEnabled(false);
+            mCallButton.setVisibility(View.GONE);
+        }
+        return v;
+    }
+
+    private void useShareCompat() {
+        //构建对象
+        ShareCompat.IntentBuilder.from(getActivity())
+                //分享内容类型
+                .setType("text/plain")
+                //设置主体
+                .setSubject(getString(R.string.crime_report_subject))
+                //设置分享内容
+                .setText(getCrimeReport())
+                //启动分享界面
+                .startChooser();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult: requestCode = " + requestCode);
+        if (requestCode == REQUEST_CODE_READ_CONTACTS) {
+            boolean success = true;
+            for (int i : grantResults) {
+                Log.d(TAG, "onRequestPermissionsResult: i = " + i);
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    success = false;
+                    break;
+                }
+            }
+            if (success) {
+                callPhone();
+            } else {
+                Toast.makeText(getContext(), R.string.need_permission, Toast.LENGTH_SHORT).show();
+            }
         }
 
-        return v;
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        //activity暂停的时候保存数据
+        //视图暂停的时候保存数据
         CrimeLab.get(getActivity())
                 .updateCrime(mCrime);
     }
@@ -158,7 +230,10 @@ public class CrimeFragment extends Fragment {
             mCrime.setDate(date);
             updateDate();
         } else if (requestCode == REQUEST_CONTACT && data != null) {
-            Uri contactUri = data.getData();
+            final Uri contactUri = data.getData();
+            if (contactUri == null) {
+                return;
+            }
             // Specify which fields you want your query to return
             // values for.
             String[] queryFields = new String[]{
@@ -168,6 +243,9 @@ public class CrimeFragment extends Fragment {
             // clause here
             Cursor c = getActivity().getContentResolver()
                     .query(contactUri, queryFields, null, null, null);
+            if (c == null) {
+                return;
+            }
             try {
                 // Double-check that you actually got results
                 if (c.getCount() == 0) {
@@ -179,6 +257,7 @@ public class CrimeFragment extends Fragment {
                 String suspect = c.getString(0);
                 mCrime.setSuspect(suspect);
                 mSuspectButton.setText(suspect);
+                mCallButton.setVisibility(View.VISIBLE);
             } finally {
                 c.close();
             }
@@ -207,5 +286,49 @@ public class CrimeFragment extends Fragment {
         String report = getString(R.string.crime_report,
                 mCrime.getTitle(), dateString, solvedString, suspect);
         return report;
+    }
+
+    private void callPhone() {
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS)) {
+            // TODO: 2019/12/2 忘记在清单文件中添加声明
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS);
+            return;
+        }
+        final Cursor cursorByName = getContext().getContentResolver().query(
+                CONTENT_URI,
+                new String[]{NUMBER},
+                DISPLAY_NAME + " = ?",
+                new String[]{mSuspectButton.getText().toString().trim()},
+                null);
+        if (cursorByName == null) {
+            return;
+        }
+        if (cursorByName.getCount() == 0) {
+            cursorByName.close();
+            Log.e(TAG, "callPhone: ", new Exception());
+            return;
+        }
+
+        if (cursorByName.moveToFirst()) {
+            final String number = cursorByName.getString(cursorByName.getColumnIndex(NUMBER));
+            cursorByName.close();
+            if (!TextUtils.isEmpty(number)) {
+                callPhone(number);
+            } else {
+                Log.e(TAG, "callPhone: ", new Exception());
+            }
+        }
+    }
+
+
+    private void callPhone(String number) {
+        try {
+            final Intent intent = new Intent(Intent.ACTION_DIAL);
+            final Uri parse = Uri.parse("tel:" + number);
+            intent.setData(parse);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getContext(), "未找到拨号应用", Toast.LENGTH_SHORT).show();
+        }
     }
 }
